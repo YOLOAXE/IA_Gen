093ff2908cdbc889fa9100ef42610001292b86b3
Albarell <Albarell>
2020-01-22 12:21:54 +0100

    12:21

--- Script/IACalcule.cs
+++ Script/IACalcule.cs
@@ -5,29 +5,17 @@ using UnityEngine;
 [System.Serializable]
 public class NeuronesCoucheD
 {
-    public float[] Perceptron = new float[10];
-    public float[,] Poids = new float[10, 10];
-    public float[] Delta = new float[10];
-}
-
-[System.Serializable]
-public class Question
-{
-    public float[] entrer = new float[10];
-    public float[] Reponce = new float[10];
+    public float[] Perceptron = new float[50];
+    public float[,] Poids = new float[50, 50];
 }
 
 public class IACalcule : MonoBehaviour
 {
     [SerializeField] private NeuronesCoucheD[] HidenNeurone = new NeuronesCoucheD[10];
-    [SerializeField] private Question[] QestValide;
-    [SerializeField] private Question[] Qest;
     [SerializeField] private float TauxDapprentissage = 0.1f;
-    [SerializeField] private float Divisseur = 100;
-    public const int Ne = 10;
+    public const int Ne = 50;
 
     private int i = 0, j = 0, k = 0;
-    private int indice = 0;
     private float result = 0;
 
     void Start()
@@ -37,31 +25,6 @@ public class IACalcule : MonoBehaviour
             HidenNeurone[i].Perceptron = new float[Ne];
             HidenNeurone[i].Poids = new float[Ne, Ne];
             HidenNeurone[i].Delta = new float[Ne];
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            GetValueExo();
-            Propagation();
-            RetroPropagation();
-            MiseAjour();
-            AplyValue();
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            indice = Random.Range(0, QestValide.Length);
-            Propagation();
-            Debug.Log(QestValide[indice].entrer[0]);
-            Debug.Log(QestValide[indice].entrer[1]);
-            Debug.Log(QestValide[indice].Reponce[0]);
-            Debug.Log(HidenNeurone[HidenNeurone.Length - 1].Perceptron[0] * Divisseur);
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            RandomizeAllPoids();
         }
     }
 
@@ -79,7 +42,7 @@ public class IACalcule : MonoBehaviour
         }
     }
 
-    void Propagation()
+    public void Propagation()
     {
         for (i = 0; i < HidenNeurone.Length - 1; i++)
         {
@@ -90,57 +53,8 @@ public class IACalcule : MonoBehaviour
                 {
                     result += HidenNeurone[i].Poids[k, j] * HidenNeurone[i].Perceptron[k];
                 }
-                HidenNeurone[i + 1].Perceptron[j] = 1 / (1 + Mathf.Exp(result));// logistique
+                HidenNeurone[i + 1].Perceptron[j] = 1 / (1 + Mathf.Exp(result));
             }
         }
-    }
-
-    void RetroPropagation()
-    {
-        for(i = 0; i < Ne; i++)
-        {
-            HidenNeurone[HidenNeurone.Length - 1].Delta[i] = (QestValide[indice].Reponce[i] / Divisseur) - HidenNeurone[HidenNeurone.Length - 1].Perceptron[i];
-        }
-        for (i = HidenNeurone.Length - 2; i >= 0; i--)
-        {
-            for (j = 0; j < Ne; j++)
-            {
-                result = 0;
-                for (k = 0; k < Ne; k++)
-                {
-                    result += HidenNeurone[i].Poids[k, j] * HidenNeurone[i + 1].Delta[j];
-                }
-                HidenNeurone[i].Delta[j] = result * (HidenNeurone[i].Perceptron[j] * (1 - HidenNeurone[i].Perceptron[j]));
-            }
-        }
-        indice++;
-        if(indice == QestValide.Length)
-        {
-            indice = 0;
-        }
-    }
-
-    void MiseAjour()
-    {
-        for (i = 0; i < HidenNeurone.Length - 1; i++)
-        {
-            for (j = 0; j < Ne; j++)
-            {
-                HidenNeurone[i].Poids[j, i] = HidenNeurone[i].Poids[j, i] + TauxDapprentissage * HidenNeurone[i].Perceptron[j] + HidenNeurone[i + 1].Delta[j];
-            }
-        }
-    }
-
-    void GetValueExo()
-    {
-        for (i = 0; i < Ne; i++)
-        {
-            HidenNeurone[0].Perceptron[i] = QestValide[indice].entrer[i] / Divisseur;
-        }
-    }
-
-    void AplyValue()
-    {
-
     }
 }
---
+++ Script/MathManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class Question
+{
+    public float[] entrer = new float[50];
+    public float[] Reponce = new float[50];
+}
+
+public class MathManager : MonoBehaviour
+{
+    [SerializeField] private GameObject IA = null;
+    [SerializeField] private int Generation = 0;
+    [SerializeField] private int nbIA = 100; //constante
+    [SerializeField] private int nbIASelectionne = 2; //constante
+    [SerializeField] private float TauxMutation = 0.1f;
+    [SerializeField] private GameObject SpawnPoint = null;
+    [SerializeField] private List<GameObject> IAR = new List<GameObject>();
+    [SerializeField] private List<GameObject> IAM = new List<GameObject>();
+    [SerializeField] private List<float> IAF = new List<float>();
+    [SerializeField] private float[,,,] BRPoids = new float[400, 5, 50, 50];
+    [SerializeField] private TextMeshProUGUI TxtGen = null;
+    public const int Ne = 50;
+
+    private int Indice = 0;
+    private int i = 0, j = 0, k = 0, s = 0, de = 0;
+
+    void Start()
+    {
+        for (i = 0; i < nbIA; i++)
+        {
+            IAR.Add(Instantiate(IA, SpawnPoint.transform.position, Quaternion.identity));
+            IAR[i].name = "Conni_" + i.ToString();
+            IAR[i].GetComponent<IACalcule>().RandomizeAllPoids();
+        }
+        Generation++;
+        TxtGen.text = "Generation: " + Generation.ToString();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FindMeilleur()
+    {
+        for(i = 0;i < IAR.Count;i++)
+        {
+            IAF.Add = IAR.GetComponent<IACalcule>().H
+            IAM.Add = IAR[i];
+        }
+    }
+}
